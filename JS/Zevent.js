7fcbe47a3644721581ac5770ee524294b9a512d1
Adekan507 <105077868+Adekan507@users.noreply.github.com>
2022-11-07 17:20:27 +0100

    Initial commit

---
+++ JS/Zevent.js
@@ -0,0 +1,109 @@
+let Streamers = {
+    "Zerator":{
+        title:"zerator",
+        img: "../images/Streamers/Zerator.jpg"
+
+    },
+    "Jeel":{
+        title:"jeeltv",
+        img:"../images/Streamers/jeel.png"
+
+    },
+    "AntoineDaniels":{
+        title:"antoinedaniel",
+        img:"../images/Streamers/antoinedaniels.png"
+
+    },
+    "MisterMv":{
+        title:"mistermv",
+        img:"../images/Streamers/mistermv.png"
+
+    },
+    "Ponce":{
+        title:"ponce",
+        img:"../images/Streamers/ponce.png"
+    },
+    "BagherasJones":{
+        title:"bagherajones",
+        img:"../images/Streamers/bagherasjones.png"
+    },
+    "Alphacast":{
+        title:"alphacast",
+        img:"../images/Streamers/alphacast.png"
+    },
+    "Ultia":{
+        title:"ultia",
+        img:"../images/Streamers/ultia.png"
+    },
+    "Moman":{
+        title:"moman",
+        img:"../images/Streamers/moman.png"
+    
+    },
+    "Avamind":{
+        title:"avamind",
+        img:"../images/Streamers/avamind.png"
+    
+    },
+    "Lapi":{
+        title:"lapi",
+        img:"../images/Streamers/lapi.png"
+    
+    },
+    "Hortyunderscore":{
+        title:"hortyunderscore",
+        img:"../images/Streamers/hortyunderscore.png"
+    
+    },
+    "Rivenzi":{
+        title:"rivenzi",
+        img:"../images/Streamers/rivenzi.png"
+    
+    },
+    "Trinity":{
+        title:"trinity",
+        img:"../images/Streamers/trinity.png"
+    
+    },
+    "Jiraya":{
+        title:"jirayalecochon",
+        img:"../images/Streamers/jiraya.png"
+    
+    },
+    "Tonton":{
+        title:"tonton",
+        img:"../images/Streamers/tonton.png"
+    
+    },
+    "AngleDroit":{
+        title:"angledroit",
+        img:"../images/Streamers/angledroit.png"
+    
+    },
+    "Altair":{
+        title:"altair",
+        img:"../images/Streamers/altair.png"
+    
+    },
+    "PapeSan":{
+        title:"papesan",
+        img:"../images/Streamers/papesan.png"
+
+    },
+    "Jeanmassiet":{
+        title:"jeanmassiet",
+        img:"../images/Streamers/jeanmassiet.jpg"
+    }
+
+    
+
+
+}
+window.onload = function() {
+    let list = document.getElementById("list");
+
+    for (let Streamer in Streamers) {
+        list.innerHTML += `<button data-game="${Streamer}" onclick="window.location.href='https://www.twitch.tv/${Streamers[Streamer].title}';" 
+            taget="_blank" style="background: url(${Streamers[Streamer].img}) no-repeat; background-size: cover;" class="Streamers_btn"></button>`;
+    }
+}
